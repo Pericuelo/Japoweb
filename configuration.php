735d1f3a43e67d191177ccae96f0fcf1f7c90633
Pere Fonolleda <peterpunkz@gmail.com>
2011-10-23 11:35:07 +0200

    Canviats els valors de la bdd per uns de default (no es pot connectar a la bdd de real des de fora).

--- configuration.php
+++ configuration.php
@@ -12,10 +12,10 @@ var $debug = '0';
 var $debug_lang = '0';
 /* Database Settings */
 var $dbtype = 'mysql';
-var $host = '70.39.233.47';
-var $user = 'eoijisho_web';
-var $password = 'myAkira21';
-var $db = 'eoijisho_main';
+var $host = 'localhost';
+var $user = 'root';
+var $password = '';
+var $db = 'japoweb';
 var $dbprefix = 'jos_';
 /* Server Settings */
 var $live_site = '';
